89c6ddcd2e60b0d1cd8389a83336df7c7a7b3b3a
Shirley DeCesari <shirley.decesari@gmail.com>
2022-08-01 14:29:07 -0700

    Feat/Test: Add relationships

--- app/models/supermarket.rb
+++ app/models/supermarket.rb
@@ -1,3 +1,5 @@
 class Supermarket < ApplicationRecord
   has_many :customers
+  has_many :customer_items, through: :customers
+  has_many :items, through: :customer_items
 end
--- spec/models/supermarket_spec.rb
+++ spec/models/supermarket_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 RSpec.describe Supermarket, type: :model do
   describe 'relationships' do
     it { should have_many :customers }
+    it { should have_many(:customer_items).through(:customers) }
+    it { should have_many(:items).through(:customer_items) }
   end
 end
