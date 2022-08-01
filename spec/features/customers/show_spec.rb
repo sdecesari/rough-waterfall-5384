736ae020784e02476682009428ab3b1b478357e7
Shirley DeCesari <shirley.decesari@gmail.com>
2022-08-01 15:34:35 -0700

    Test: User story 2

--- spec/features/customers/show_spec.rb
+++ spec/features/customers/show_spec.rb
@@ -30,4 +30,22 @@ RSpec.describe 'Customer Show Page' do
     expect(page).to have_content("Salmon")
     expect(page).to_not have_content("Capers")
   end 
+
+  it "can show total price of all items" do 
+    supermarket1 = Supermarket.create!(name: "Raileys", location: "Sparks")
+
+    customer1 = Customer.create!(name: "Shirley", supermarket_id: supermarket1.id)
+
+    item1 = Item.create!(name: "Bagels", price: 3)
+    item2 = Item.create!(name: "Cream Cheese", price: 2)
+    item3 = Item.create!(name: "Salmon", price: 5)
+
+    customer_item1 = CustomerItem.create!(customer_id: customer1.id, item_id: item1.id)
+    customer_item2 = CustomerItem.create!(customer_id: customer1.id, item_id: item2.id)
+    customer_item3 = CustomerItem.create!(customer_id: customer1.id, item_id: item3.id)
+
+    visit "/customers/#{customer1.id}"
+
+    expect(page).to have_content("Total Price: $10")
+  end 
 end 
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -6,4 +6,22 @@ RSpec.describe Customer, type: :model do
     it { should have_many :customer_items }
     it { should have_many(:items).through(:customer_items) }
   end
+
+  describe "class methods" do 
+    it "#total_price" do 
+      supermarket1 = Supermarket.create!(name: "Raileys", location: "Sparks")
+
+    customer1 = Customer.create!(name: "Shirley", supermarket_id: supermarket1.id)
+
+    item1 = Item.create!(name: "Bagels", price: 3)
+    item2 = Item.create!(name: "Cream Cheese", price: 2)
+    item3 = Item.create!(name: "Salmon", price: 5)
+
+    customer_item1 = CustomerItem.create!(customer_id: customer1.id, item_id: item1.id)
+    customer_item2 = CustomerItem.create!(customer_id: customer1.id, item_id: item2.id)
+    customer_item3 = CustomerItem.create!(customer_id: customer1.id, item_id: item3.id)
+
+    expect(customer1.total_price).to eq(10)
+    end 
+  end
 end
